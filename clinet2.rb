be37975e61173c07fdc44ea62bfdf92082a2212a
Alexander Lenhardt <alenhard@techfak.uni-bielefeld.de>
2012-01-23 18:20:14 +0100

    removed some comments

--- clinet2.rb
+++ clinet2.rb
@@ -5,13 +5,8 @@ require 'rinda/tuplespace'
 DRb.start_service
 ts=DRbObject.new(nil, ARGV.shift)
 
-# notification types: 'write', 'take', & 'delete'.  Event type 'close'
-# if entry expires..  Second arg is wild card for entries we want to
-# watch.
 observer = ts.notify('write', [:page,nil])
 
-# Most people fire off a thread to do the watching, but that is all we
-# are gonna do in this program...
 i=0
 observer.each do |event, tuple|
   puts("OBJ: #{event.inspect} #{tuple.inspect}")
--- server.rb
+++ server.rb
@@ -5,8 +5,6 @@ require 'rinda/tuplespace'
 # Construct our tuple space 
 ts = Rinda::TupleSpace.new
 
-# Start a drb server attached to the tuple space A URI string may be
-# given: "druby://foobar.dal.design.ti.com:1234" instead of nil.
 DRb.start_service("druby://localhost:12345", ts) 
 
 puts("Server Running: #{DRb.uri}")
