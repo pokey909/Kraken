1c4a02535cc424f41b0b67fa2e4031105c9af6ad
Alexander Lenhardt <alenhard@techfak.uni-bielefeld.de>
2012-01-27 00:01:54 +0100

    changed to rabbitmq

--- main.rb
+++ main.rb
@@ -2,19 +2,13 @@
 require 'rubygems'
 require 'mongo'
 require 'open-uri'
+require 'yaml'
+require 'amqp'
 
 class PageProcessor
 
 	def initialize()
-		@db = Mongo::Connection.new.db("mydb")
-		@db = Mongo::Connection.new("localhost").db("kraken")
-		@connection = Mongo::Connection.new # (optional host/port args)
-		@connection.database_names.each { |name| puts name }
-		@connection.database_info.each { |info| puts info.inspect}
-	  @links=@db["links"]
-	  @pages=@db["pages"]
     @procs=[]
-    self.clearLinks
     1000.times { @links.insert( {:url=>"http://www.ruby-doc.org/core-1.9.3/Process.html", :processing=>false, :done=>false, :pid=>0 } )}
 	end
 
@@ -30,18 +24,51 @@ class PageProcessor
     @pages.remove
   end
 
+  
   def process
     pid = Process.pid
-    Signal.trap("HUP") { puts "PageProcessor #{pid} exiting..."; exit; }
-    loop do
-      while (res = @links.find_and_modify( { :query=>{:processing=>false, :done=>false}, :update=>{"$set"=>{:processing=>true, :pid=>pid}}, :new=>true  } )) do
-        ## download page
-        @pages.insert({ :body=>get(res["url"]), :url=>res["url"], :pid=>res["pid"]})
-        @links.update({"_id"=>res["_id"]}, {"$set"=>{:processing=>false, :done=>true}})
+    t = Thread.new { EventMachine.run }
+    sleep(0.5)
+    connection = AMQP.connect
+    channel    = AMQP::Channel.new(connection, :auto_recovery => true)
+    channelOut = AMQP::Channel.new(connection, :auto_recovery => true)
+    channel.prefetch(1)
+    #exchange   = channel.fanout("kraken.", :durable => true, :auto_delete => false)
+
+    channel.queue("kraken.links.pending", :durable => true, :auto_delete => false).subscribe(:ack => true) do |metadata, payload|
+      case metadata.type
+      when "isome command"
+        data = YAML.load(payload)
+        puts "[gems.install] Received a 'gems.install' request with #{data.inspect}"
+
+        # just to demonstrate a realistic example
+        shellout = "gem install #{data[:gem]} --version '#{data[:version]}'"
+        puts "[gems.install] Executing #{shellout}"; system(shellout)
+
+        puts
+        puts "[gems.install] Done"
+        puts
+      else
+        puts "[commands] Unknown command: #{metadata.type}"
+        body = get("some url")
+        payload = { :url => 'some url', :body => body, :on_page => @blocks[:on_page].call(body) }.to_yaml
+        channel.default_exchange.publish(payload,
+                                         :type        => "page post",
+                                         :routing_key => "kraken.pages.pending")
       end
+
+      # message is processed, acknowledge it so that broker discards it
+      metadata.ack
     end
+    puts "[PageProcessor] ##{pid} booted."
+    Signal.trap("INT") { connection.close { EventMachine.stop } }
+    t.join
   end
   
+  def on_page(&block)
+    @blocks[:on_page] = block
+  end
+
   def spawn(num)
     num.times { @procs << fork { self.process } }
   end
